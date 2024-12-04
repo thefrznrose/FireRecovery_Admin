7096d814ff8fe3686e9dc86b13f0e0bcf173871d
Shaun Rose <thefrznrose@gmail.com>
2024-12-04 15:37:23 -0800

    It works on my machine

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ export default withBundleAnalyzer({
         source: "/(.*)",
         headers: [
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          // { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
         ],
       },
     ];
